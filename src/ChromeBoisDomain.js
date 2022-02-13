4af2dbd106216714e764c322e5befaf8b59911af
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-12 19:38:26 -0500

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -4,30 +4,25 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 export default class ChromeBoisDomain extends Component {
   
+    
   handleMouseMove = (event) => {
-    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
-     * from the event and use them to invoke the `drawChromeBoiAtCoords`
-     * function that has been provided and is already imported
-     * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
-     */
+   drawChromeBoiAtCoords (event.clientX, event.clientY)
+    //invokes the `drawChromeBoiAtCoords` method within `handleMouseMove`, 
+    //passing the captured x and y values of the mouse from the event
   }
-  
-  /* TODO: Create an event handler which, when fired, invokes the provided
-   * `toggleCycling` function with no arguments. Don't forget the click event
-   * listener that should fire it!
-   */
-   
-  /* TODO: Add an event listener to the `<canvas>` element to capture when a key
-  /* is pressed. When a key is pressed, an event handler should invoke the
-  /* provided `resize` function with a single argument of either '+' or '-'
-  /* if the key pressed was 'a', then it should call `resize` with '+'
-  /* if the key pressed was 's', then it should call `resize` with '-' 
-   */
-  
+  handleKeyDown = (event) => {
+    if (event.key === 'a') {
+      resize('+') // when the 'a' key is pressed, `resize` is invoked with the argument of '+'
+    } else if (event.key === 's') {
+      resize('-') //when the 's' key is pressed, `resize` is invoked with the argument of '-'
+    }
+  }
   render() {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+        onClick={() => {toggleCycling()}} //has an event listener for clicks on the <canvas> element that triggers `toggleCycling`
+        onKeyDown={this.handleKeyDown} //has an event listener for key presses on the <canvas> element that triggers `resize`
         width='900'
         height='600'
         tabIndex="0">
